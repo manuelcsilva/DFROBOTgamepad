639115f9a93a63b9ec50848dd83919f3926583cf
Manuel Silva <82286102+manuelcsilva@users.noreply.github.com>
2024-02-27 11:14:55 +0000

    add buttons

--- custom.ts
+++ custom.ts
@@ -17,6 +17,9 @@ enum Eixo {
     //% block="Y"
     Y
 }
+
+
+
 /**
  * Custom blocks
  */
@@ -28,6 +31,34 @@ namespace GamePad {
      * @param s describe parameter here, eg: "Hello"
      * @param e describe parameter here
      */
+    //% blockId=initJoystickBit block="initialization joystick:bit"
+    export function initJoystickBit(): void {
+        pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+    }
+
+    export enum ButtonsPin {
+        //% block="C"
+        P13 = DAL.MICROBIT_ID_IO_P13,
+        //% block="D"
+        P14 = DAL.MICROBIT_ID_IO_P14,
+        //% block="E"
+        P15 = DAL.MICROBIT_ID_IO_P15,
+        //% block="F"
+        P16 = DAL.MICROBIT_ID_IO_P16
+    }
+    export enum ButtonType {
+        //% block="pressed"
+        down = PulseValue.High,
+        //% block="released"
+        up = PulseValue.Low
+    }
+    //% block
+    export function Butao(button: ButtonsPin, event: ButtonType, handler: Action): void {
+        pins.onPulsed(<number>button, <number>event, handler);
+    }
     //% block
     export function Vibracao(t: Estado): void {
         if (t == Estado.On) {
--- main.ts
+++
@@ -1,6 +0,0 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
-basic.forever(function () {
-    basic.showNumber(GamePad.joystick(Eixo.X))
-})
